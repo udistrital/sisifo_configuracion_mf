43f2d94f73bb0d7c7ca4c8376ec2ae861f39362e
alexhrr <alexh.2401@hotmail.com>
2024-11-27 21:02:23 -0500

    feat: Se agrega la validacion de usuarios creados y el estado del periodo antes de registrar otro con el mismo rol udistrital/sisifo_documentacion#301

--- src/app/modules/gestion-usuarios/registrar-usuario/registrar-usuario.component.ts
+++ src/app/modules/gestion-usuarios/registrar-usuario/registrar-usuario.component.ts
@@ -1,3 +1,4 @@
+import { Rol, Usuario } from './../../gestion-roles/utils/gestion-usuarios.models';
 import { AutenticacionService } from './../../../services/autenticacion.service';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
@@ -31,7 +32,6 @@ export class RegistrarUsuarioComponent {
     private terceros_service: TercerosService,
     private autenticacionService: AutenticacionService,
     private modalService: ModalService
-    
   ) {}
 
   ngOnInit(): void {
@@ -44,7 +44,7 @@ export class RegistrarUsuarioComponent {
         if (response && Array.isArray(response.Data)) {
           this.roles = response.Data;
         } else {
-          console.error( 
+          console.error(
             'La respuesta no contiene una propiedad Data que sea un array.'
           );
           this.roles = [];
@@ -53,7 +53,11 @@ export class RegistrarUsuarioComponent {
       },
       error: (err: any) => {
         console.error('Error al obtener roles:', err);
-        this.modalService.mostrarModal('Error al obtener los roles. Por favor, intenta nuevamente.', 'warning','error');
+        this.modalService.mostrarModal(
+          'Error al obtener los roles. Por favor, intenta nuevamente.',
+          'warning',
+          'error'
+        );
         this.loading = false;
       },
     });
@@ -73,45 +77,157 @@ export class RegistrarUsuarioComponent {
     };
     const nombreRol = this.roles.find((r) => r.Id === rolId)?.Nombre || '';
     this.loading = true;
-    this.historico_service.post('usuarios/', usuario).subscribe({
+    console.log('documento', documento);
+
+    this.historico_service.get(`usuarios?query=documento:${documento}`).subscribe({
       next: (response: any) => {
-        const periodo = {
-          FechaFin: fechaFinFormato,
-          FechaInicio: fechaInicioFormato,
-          Finalizado: false,
-          RolId: { Id: rolId },
-          UsuarioId: { Id: response.Data.Id },
-        };
-        this.historico_service
-          .post('periodos-rol-usuarios/', periodo)
-          .subscribe({
-            next: (response: any) => {
-              console.log('Periodo creado:', response);
-              this.loading = false;
+        console.log('response', response);
+  
+        if (response && response.Data && response.Data.length > 0) {
+          console.log('Usuario existente:', response.Data[0]);
+  
+          // Verificar periodos del usuario
+          this.historico_service
+            .get(`usuarios/${documento}/periodos`)
+            .subscribe({
+              next: (periodosResponse: any) => {
+                console.log('Periodos del usuario:', periodosResponse);
+                
+                if (
+                  periodosResponse &&
+                  periodosResponse.Data &&
+                  periodosResponse.Data.length > 0 &&
+                  periodosResponse.Data[0].RolId.Id === rolId &&
+                  periodosResponse.Data[0].Finalizado === true
+
+                 ) {
+                  this.crearPeriodoRol(
+                    periodosResponse.Data[0].UsuarioId.Id,
+                    fechaInicioFormato,
+                    fechaFinFormato,
+                    rolId,
+                    nombreRol,
+                    email
+                  );
+                
+                } else {
+                  this.modalService.mostrarModal(
+                    'El usuario ya tiene vigente el rol asignado.',
+                    'warning',
+                    'error'
+                  );
+                  this.loading = false;
+                }
+              },
+              error: (err: any) => {
+                console.error('Error al verificar periodos:', err);
+                this.modalService.mostrarModal(
+                  'Error al verificar los periodos del usuario.',
+                  'warning',
+                  'error'
+                );
+                this.loading = false;
+              },
+              complete: () => {
+                this.loading = false;
+              },
+            });
+        } else {
+          console.log('Usuario no existente');
+          this.historico_service.post('usuarios/', usuario).subscribe({
+            next: (usuarioResponse: any) => {
+              console.log('Usuario creado:', usuarioResponse);
+              this.crearPeriodoRol(
+                usuarioResponse.Data.Id,
+                fechaInicioFormato,
+                fechaFinFormato,
+                rolId,
+                nombreRol,
+                email
+              );
             },
             error: (err: any) => {
-              console.error('Error al crear periodo:', err);
-              this.modalService.mostrarModal('Error al crear el periodo para el usuario.', 'warning','error');
+              console.error('Error al crear usuario:', err);
+              this.modalService.mostrarModal(
+                'Error al crear el usuario. Verifica los datos e intenta nuevamente.',
+                'warning',
+                'error'
+              );
               this.loading = false;
             },
           });
+        }
+      },
+      error: (err: any) => {
+        console.error('Error al verificar usuario:', err);
+        this.modalService.mostrarModal(
+          'Error al verificar el usuario. Verifica los datos e intenta nuevamente.',
+          'warning',
+          'error'
+        );
+        this.loading = false;
+      },
+      complete: () => {
+        this.loading = false;
+      },
+    });
+  }
+
+  private crearPeriodoRol(
+    ususarioId: number,
+    fechaInicio: string,
+    fechaFin: string,
+    rolId: number,
+    nombreRol: string,
+    email: string
+  ) {
+    const periodo = {
+      FechaFin: fechaFin,
+      FechaInicio: fechaInicio,
+      finalizado: false,
+      RolId: { Id: rolId },
+      UsuarioId: { Id: ususarioId },
+    };
+
+
+    this.historico_service.post('periodos-rol-usuarios/', periodo).subscribe({
+      next: (response: any) => {
+        console.log('Periodo creado:', response);
+        // this.modalService.mostrarModal(
+        //   'Usuario, periodo y rol creados exitosamente.',
+        //   'success',
+        //   'Creado'
+        // );
         this.autenticacionService
           .PostRol('rol/add', nombreRol, email)
           .subscribe({
             next: (response: any) => {
-              console.log('Rol creado:', response);
-              this.loading = false;
-              this.modalService.mostrarModal('Usuario, periodo y rol creados exitosamente.', 'success', 'Creado');
+              console.log('Rol asignado:', response);
+              this.modalService.mostrarModal(
+                'Rol asignado exitosamente.',
+                'success',
+                'Creado'
+              );
             },
             error: (err: any) => {
-              console.error('Error al crear rol:', err);
-              this.modalService.mostrarModal('Error al asignar el rol al usuario.', 'warning','error');
+              console.error('Error al asignar rol:', err);
+              this.modalService.mostrarModal(
+                'Error al asignar el rol al usuario.',
+                'warning',
+                'error'
+              );
             },
           });
       },
       error: (err: any) => {
-        console.error('Error al crear usuario:', err);
-        this.modalService.mostrarModal('Error al crear el usuario. Verifica los datos e intenta nuevamente.', 'warning','error');
+        console.error('Error al crear periodo:', err);
+        this.modalService.mostrarModal(
+          'Error al crear el periodo. Verifica los datos e intenta nuevamente.',
+          'warning',
+          'error'
+        );
+      },
+      complete: () => {
         this.loading = false;
       },
     });
@@ -132,12 +248,20 @@ export class RegistrarUsuarioComponent {
             this.nombreCompleto = data[0].Tercero.NombreCompleto;
             this.loading = false;
           } else {
-            this.modalService.mostrarModal('No se encontraron datos del usuario con el documento proporcionado.', 'warning','error');
-            this.loading = false;
+            this.modalService.mostrarModal(
+              'No se encontraron datos del usuario con el documento proporcionado.',
+              'warning',
+              'error'
+            );
+            //this.loading = false;
           }
         },
         error: (err: any) => {
-          this.modalService.mostrarModal('No se encontraron datos del usuario ingresado', 'warning','error');
+          this.modalService.mostrarModal(
+            'No se encontraron datos del usuario ingresado',
+            'warning',
+            'error'
+          );
           this.loading = false;
         },
       });
@@ -145,9 +269,18 @@ export class RegistrarUsuarioComponent {
 
   BuscarDocumento(documento: string) {
     if (!documento) {
-      this.modalService.mostrarModal('Por favor, ingresa un documento válido.', 'warning','error');
+      this.modalService.mostrarModal(
+        'Por favor, ingresa un documento válido.',
+        'warning',
+        'error'
+      );
       return;
     }
+
+    this.nombreCompleto = '';
+    this.emailInput.nativeElement.value = '';
+    
+
     this.loading = true;
     this.autenticacionService
       .getDocumento(`token/documentoToken`, documento)
@@ -160,12 +293,20 @@ export class RegistrarUsuarioComponent {
             this.emailInput.nativeElement.value = data.email;
             this.loading = false;
           } else {
-            this.modalService.mostrarModal('No se encontraron datos del documento proporcionado.', 'warning','error');
+            this.modalService.mostrarModal(
+              'No se encontraron datos del documento proporcionado.',
+              'warning',
+              'error'
+            );
             this.loading = false;
           }
         },
         error: (err: any) => {
-          this.modalService.mostrarModal('No se encontraron datos del usuario ingresado', 'warning','error');
+          this.modalService.mostrarModal(
+            'No se encontraron datos del usuario ingresado',
+            'warning',
+            'error'
+          );
           this.loading = false;
         },
       });
@@ -173,9 +314,18 @@ export class RegistrarUsuarioComponent {
 
   BuscarCorreo(correo: string) {
     if (!correo) {
-      this.modalService.mostrarModal('Por favor, ingresa un correo válido.', 'warning','error');
+      this.modalService.mostrarModal(
+        'Por favor, ingresa un correo válido.',
+        'warning',
+        'error'
+      );
       return;
     }
+
+    this.nombreCompleto = '';
+    this.emailInput.nativeElement.value = '';
+    this.identificacion = '';
+
     this.loading = true;
     this.autenticacionService.getEmail(`token/userRol`, correo).subscribe({
       next: (data: any) => {
@@ -186,15 +336,22 @@ export class RegistrarUsuarioComponent {
           this.documentoInput.nativeElement.value = this.identificacion;
           this.loading = false;
         } else {
-          this.modalService.mostrarModal('No se encontraron datos asociados al correo proporcionado.', 'warning','error');
+          this.modalService.mostrarModal(
+            'No se encontraron datos asociados al correo proporcionado.',
+            'warning',
+            'error'
+          );
           this.loading = false;
         }
       },
       error: (err: any) => {
-        this.modalService.mostrarModal('Error al buscar el correo. Verifica los datos e intenta nuevamente.', 'warning','error');
+        this.modalService.mostrarModal(
+          'Error al buscar el correo. Verifica los datos e intenta nuevamente.',
+          'warning',
+          'error'
+        );
         this.loading = false;
       },
     });
   }
-
 }
