b795a92649c421d9e02cb902f16f1bbab6ebd80c
unknown <collegek450@gmail.com>
2024-11-29 21:40:20 -0500

    feat: udistrital/sisifo_documentacion#294 Cambio de interfaces

--- src/app/modules/gestion-usuarios/gestion-usuarios.module.ts
+++ src/app/modules/gestion-usuarios/gestion-usuarios.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { HttpClientModule,  provideHttpClient } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { RegistrarUsuarioComponent } from './registrar-usuario/registrar-usuario.component';
 import { GestionUsuariosRoutingModule } from './gestion-usuarios-routing.module';
 
@@ -12,10 +12,10 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatDatepickerModule} from '@angular/material/datepicker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { NgFor } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
+import { MatCard, MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -26,13 +26,12 @@ import { TercerosService } from 'src/app/services/terceros.service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatPaginatorModule } from '@angular/material/paginator';
 
-
 @NgModule({
   declarations: [
     RegistrarUsuarioComponent,
     ActualizarUsuarioComponent,
     UsuariosComponent,
-    ],
+  ],
   imports: [
     CommonModule,
     FormsModule,
@@ -52,11 +51,8 @@ import { MatPaginatorModule } from '@angular/material/paginator';
     MatFormFieldModule,
     MatDialogModule,
     MatProgressSpinnerModule,
-    MatPaginatorModule
+    MatPaginatorModule,
   ],
-  providers:[
-    HistoricoUsuariosMidService,
-    TercerosService
-  ]
+  providers: [HistoricoUsuariosMidService, TercerosService],
 })
-export class GestionUsuariosModule { }
+export class GestionUsuariosModule {}
