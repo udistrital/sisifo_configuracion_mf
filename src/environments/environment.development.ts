e124af4284b212abccb58a75bb50c2a829d72009
Heiner Edwar Granados Leal <50187485+edwargl7@users.noreply.github.com>
2025-01-01 15:57:10 -0500

    feat: Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     SISTEMA_INFORMACION_ID: 6816,
-    ROLES_CONSULTA: ['JEFE_DEPENDENCIA', 'AUDITOR', 'EJECUTOR', 'ESTUDIANTE'],
-    ROLES_CONSULTA_EDICION: ['ADMIN_SISIFO', 'SOPORTE_OAS', 'ADMIN_SGA'],
+    ROLES_CONSULTA: ['JEFE_CONTROL_INTERNO', 'AUDITOR_EXPERTO'],
+    ROLES_CONSULTA_EDICION: ['ADMIN_SISIFO'],
     AUTENTICACION_MID: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/',
     HISTORICO_USUARIOS_MID: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/usuarios_rol_crud/v1/', 
     TERCEROS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/terceros_crud/v1/',
